653c2fc095fafe4daa76d19ce1d2e9141adbff47
Anatoliy <tolxpams@gmail.com>
2022-03-04 16:52:29 +0200

    login view controller

---
+++ photed/photed/Resources/EnvData.swift
@@ -0,0 +1,22 @@
+//
+//  EnvData.swift
+//  photed
+//
+//  Created by Anatoliy Khramchenko on 04.03.2022.
+//
+
+import Foundation
+import UIKit
+
+class EnvData {
+    
+    static let paddingLeft: CGFloat = 10
+    static let paddingUp: CGFloat = 40
+    static let labelHeight: CGFloat = 40
+    static let blockDistance: CGFloat = 20
+    static let textFieldHeight: CGFloat = 20
+    static let paddingDown: CGFloat = 20
+    static let buttonHeight: CGFloat = 40
+    static let littleButtonHeight: CGFloat = 20
+    
+}
